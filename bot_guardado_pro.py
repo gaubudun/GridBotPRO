67940cf220f67f3424ad7869c3e55274d9a61aeb
gaubudun <djdanielheras@gmail.com>
2026-01-31 01:44:30 +0100

    Commit inicial GridBotPRO Terminal Ultra

---
+++ bot_guardado_pro.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+print("🚀 GridBotPRO Terminal Ultra iniciado")
---
+++ install_gridbot_pro.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Instalando GridBotPRO..."
+pkg install -y python
+pip install requests
+echo "Instalado"
